a4ce05895a92f5b5eb84bc08036096fd008d34b9
Victor Nani <123387218+AristarhMD@users.noreply.github.com>
2026-09-22 23:05:46 +0000

    Add comment
    Add comment section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,34 @@
-import data from "../data.js"
-import Post from "./reusable/Post.jsx"
+import data from "../data.js";
+import Post from "./reusable/Post.jsx";
 
 function App() {
   return (
     <main>
       <section className="mx-auto w-[91.46%] py-8 flex flex-col gap-4">
-        {data.map((post, idx) => <Post key={idx} {...post}/>)}
+        {data.map((post, idx) => (
+          <Post key={idx} {...post} />
+        ))}
+      </section>
+
+      <section className="mx-auto w-[91.46%]">
+        <div className="bg-white p-4 rounded-lg grid grid-cols-[repeat(2, min-content)] grid-rows-[repeat(2,min-content)] gap-y-4">
+          <textarea
+            className="col-start-1 col-span-2 py-2 px-4 border border-inside border-grey-100 rounded-lg h-24 resize-none preset-2-r text-grey-800 placeholder:text-grey-500"
+            placeholder="Add a comment…"
+          />
+
+          <img
+            className="size-8 col-start-1 self-center"
+            src="./avatars/image-juliusomo.png"
+          />
+
+          <button className="py-3 px-8 col-start-2 preset-2-m justify-self-end bg-purple-600 hover:bg-purple-200 text-white rounded-lg cursor-pointer">
+            SEND
+          </button>
+        </div>
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/reusable/Post.jsx
+++ src/components/reusable/Post.jsx
@@ -4,8 +4,8 @@ export default function Post({ avatar, user, posted, text, likes, replay }) {
   const replyIcon = (
     <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg">
       <path
+        className="fill-[#5357B6] group-active:fill-[#c5c6ef] group-hover:fill-[#c5c6ef]"
         d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z"
-        fill="#5357B6"
       />
     </svg>
   );
@@ -13,8 +13,8 @@ export default function Post({ avatar, user, posted, text, likes, replay }) {
   const deleteIcon = (
     <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
       <path
+        className="fill-[#ED6368] group-active:fill-[#ffb8bb] group-hover:fill-[#ffb8bb]"
         d="M1.167 12.448c0 .854.7 1.552 1.555 1.552h6.222c.856 0 1.556-.698 1.556-1.552V3.5H1.167v8.948Zm10.5-11.281H8.75L7.773 0h-3.88l-.976 1.167H0v1.166h11.667V1.167Z"
-        fill="#ED6368"
       />
     </svg>
   );
@@ -22,8 +22,8 @@ export default function Post({ avatar, user, posted, text, likes, replay }) {
   const editIcon = (
     <svg width="14" height="14" xmlns="http://www.w3.org/2000/svg">
       <path
+        className="fill-[#5357B6] group-active:fill-[#c5c6ef] group-hover:fill-[#c5c6ef]"
         d="M13.479 2.872 11.08.474a1.75 1.75 0 0 0-2.327-.06L.879 8.287a1.75 1.75 0 0 0-.5 1.06l-.375 3.648a.875.875 0 0 0 .875.954h.078l3.65-.333c.399-.04.773-.216 1.058-.499l7.875-7.875a1.68 1.68 0 0 0-.061-2.371Zm-2.975 2.923L8.159 3.449 9.865 1.7l2.389 2.39-1.75 1.706Z"
-        fill="#5357B6"
       />
     </svg>
   );
@@ -45,7 +45,7 @@ export default function Post({ avatar, user, posted, text, likes, replay }) {
 
         <div className="flex items-center justify-between">
           <LikesBtn>{likes}</LikesBtn>
-          <button className="cursor-pointer flex items-center gap-2 preset-2-m text-purple-600">
+          <button className="group cursor-pointer flex items-center gap-2 preset-2-m text-purple-600 hover:text-purple-200">
             {replyIcon} <span>Reply</span>
           </button>
         </div>
@@ -54,7 +54,10 @@ export default function Post({ avatar, user, posted, text, likes, replay }) {
       {replay && (
         <div className="pl-4 flex flex-col gap-4 border-l-2 border-grey-100">
           {replay.map((replies, idx) => (
-            <div key={idx} className="bg-white p-4 rounded-lg grid grid-cols-1 grid-rows-[repeat(3,min-content)] gap-y-4">
+            <div
+              key={idx}
+              className="bg-white p-4 rounded-lg grid grid-cols-1 grid-rows-[repeat(3,min-content)] gap-y-4"
+            >
               <div className="flex items-center gap-4">
                 <img
                   className="size-8"
@@ -78,17 +81,17 @@ export default function Post({ avatar, user, posted, text, likes, replay }) {
                 <LikesBtn>{replies.likes}</LikesBtn>
                 {replies.user === "juliusomo" ? (
                   <div className="preset-2-m flex items-center gap-4">
-                    <button className="cursor-pointer flex items-center gap-2 text-pink-400">
+                    <button className="group cursor-pointer flex items-center gap-2 text-pink-400 hover:text-pink-200">
                       {deleteIcon}
                       <span>Delete</span>
                     </button>
-                    <button className="cursor-pointer flex items-center gap-2 text-purple-600">
+                    <button className="group cursor-pointer flex items-center gap-2 text-purple-600 hover:text-purple-200">
                       {editIcon}
                       <span>Edit</span>
                     </button>
                   </div>
                 ) : (
-                  <button className="cursor-pointer flex items-center gap-2 preset-2-m text-purple-600">
+                  <button className="group cursor-pointer flex items-center gap-2 preset-2-m text-purple-600 hover:text-purple-200">
                     {replyIcon} <span>Reply</span>
                   </button>
                 )}
